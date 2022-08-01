dbf224f76aeaf991297cb5db7dd0e22e2961b73d
tibaredha <tibaredha@yahoo.fr>
2022-08-01 19:59:11 +0100

    maj

--- sig.R
+++ sig.R
@@ -1,7 +1,7 @@
 #load data from shape file ----
 library(sf)
-w_algeria <- read_sf("../gadm41_DZA_shp/gadm41_DZA_1.shp",stringsAsFactors = TRUE)
-wc_algeria <- read_sf("../gadm41_DZA_shp/gadm41_DZA_2.shp")
+w_algeria <- read_sf("shp/gadm41_DZA_1.shp",stringsAsFactors = TRUE)
+wc_algeria <- read_sf("shp/gadm41_DZA_2.shp")
 
 #preprocessing ----
 
@@ -16,7 +16,7 @@ wilaya_x <- function(wilaya_n){
     filter(NAME_1==wilaya_n)#Djelfa Médéa
   
   ggplot()+
-    geom_sf(data = wilaya,fill="green",color="black",size=0.25)+  ##,aes(fill=CC_2)
+    geom_sf(data = wilaya,fill="lightgreen",color="black",size=0.25)+  ##,aes(fill=CC_2)
     theme_void()+
     coord_sf(crs = "+proj=robin")
 }
