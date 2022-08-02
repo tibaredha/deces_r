840d2db575f0fe39053469b0f74cd2ca35d0ff48
tibaredha <tibaredha@yahoo.fr>
2022-08-02 22:41:11 +0100

    add djelfa shp

--- sig.R
+++ sig.R
@@ -1,12 +1,32 @@
 #load data from shape file ----
 library(sf)
 w_algeria <- read_sf("shp/gadm41_DZA_1.shp",stringsAsFactors = TRUE)
+#st_write(obj = w_algeria, dsn = "gpkg/w_algeria.gpkg", layer = "w_algeria", delete_layer = TRUE)
+
+
 wc_algeria <- read_sf("shp/gadm41_DZA_2.shp")
+#st_write(obj = wc_algeria, dsn = "gpkg/wc_algeria.gpkg", layer = "wc_algeria", delete_layer = TRUE)
 
 #preprocessing ----
+#create shp for wilaya de djelfa for simplifing trt
+
+#library(dplyr)
+
+#wc_algeria %>% 
+  #names() 
+
+#wc_algeria %>% 
+  #select(NAME_1,NAME_2,CC_2) %>% 
+  #filter(NAME_1 == "Djelfa") -> djelfa 
+  #View()
+
+#View(djelfa)
+
+#st_write(obj = djelfa, dsn = "gpkg/djelfa.gpkg", layer = "djelfa", delete_layer = TRUE)
+#st_write(obj = djelfa, "shp/djelfa.shp", delete_layer = TRUE)
 
 
-
+#plot(djelfa["CC_2"])
 
 
 #fonction ----
